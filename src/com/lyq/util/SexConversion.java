102339465168ed187cbe5b60bf78d69c6c4e14df
姜泉 <595121869@qq.com>
2014-05-29 15:12:52 +0800

    网上原代码
    网上原代码

---
+++ src/com/lyq/util/SexConversion.java
@@ -0,0 +1,34 @@
+package com.lyq.util;
+
+import java.util.Map;
+import ognl.DefaultTypeConverter;
+import org.apache.struts2.util.StrutsTypeConverter;
+import java.util.List;
+import java.util.ArrayList;
+
+import com.lyq.model.Sex;
+/**
+ * 性别类型转换器
+ * @author Li Yongqiang
+ */
+public class SexConversion extends StrutsTypeConverter {
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Object convertFromString(Map arg0, String[] arg1, Class arg2) {
+		Sex sexs[] = Sex.values();
+		for (Sex sex : sexs) {
+			if(sex.getName().equals(arg1[0])){
+				return sex;
+			}
+		}
+		return null;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public String convertToString(Map arg0, Object arg1) {
+		Sex sex = (Sex) arg1;
+		return sex.getName();
+	}
+}
